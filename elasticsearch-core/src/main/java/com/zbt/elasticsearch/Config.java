86b5daa82dd946e836815a7a2f9ffe78bce5c26d
张博韬 <botao.zhang@dianping.com>
2016-01-31 04:02:45 +0800

    remove data

--- elasticsearch-core/src/main/java/com/zbt/elasticsearch/Config.java
+++ elasticsearch-core/src/main/java/com/zbt/elasticsearch/Config.java
@@ -1,10 +1,19 @@
 package com.zbt.elasticsearch;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
@@ -15,9 +24,45 @@ import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 @EnableElasticsearchRepositories(basePackages = {"com.zbt.elasticsearch.repository"})
 public class Config {
 
+    @Value("${elasticsearch.port}")
+    Integer port;
+    @Value("${elasticsearch.host}")
+    String host;
+    @Value("${elasticsearch.cluster.name}")
+    String clusterName;
+
+    /**
+     * 启动在本地
+     * @return ElasticsearchOperations
+     */
     @Bean
     public ElasticsearchOperations elasticsearchTemplate() {
         return new ElasticsearchTemplate(nodeBuilder().loadConfigSettings(false)
         .local(true).node().client());
     }
+
+    /**
+     * Transport Client（连接远程es服务）
+     * @return ElasticsearchOperations
+     */
+/*    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchTemplate(client());
+    }*/
+
+/*    @Bean
+    public Client client(){
+        Settings settings = Settings.builder()
+                .put("cluster.name",clusterName)
+                .build();
+        TransportClient client= TransportClient.builder().settings(settings).build();
+        TransportAddress address = null;
+        try {
+            address = new InetSocketTransportAddress(InetAddress.getByName(host), port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        client.addTransportAddress(address);
+        return client;
+    }*/
 }
